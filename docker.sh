50524b5b26edcf0678ec0309383ab4a6fa39942b
Makalfo <info@chthonia.io>
2021-12-07 21:11:42 +0000

    update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,4 +4,4 @@ docker run -d \
     -e GOOGLE_APPLICATION_CREDENTIALS="/bigquery-api.json" \
 	--mount "type=bind,source=/git/mina-telegram-alert/bigquery-api.json,dst=/bigquery-api.json,readonly" \
 	--mount "type=bind,source=/git/mina-telegram-alert/config.ini,dst=/mina-telegram-alert/config.ini" \
-	makalfe/mina-telegram-alerts
+	makalfe/mina-telegram-alerts:latest
