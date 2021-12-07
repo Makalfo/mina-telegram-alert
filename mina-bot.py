2af54816b5f823ea90d0b7a9029f20ca22d55d47
Makalfo <info@chthonia.io>
2021-12-07 05:45:14 +0000

    push

--- mina-bot.py
+++ mina-bot.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
-
 import requests, configparser
 import os
+import pandas as pd
+import json
+import base58
+import time
+from google.cloud import bigquery
+
+pd.options.mode.chained_assignment = None 
+
+# Mina constants
+MINA_DECIMALS = 1 / 1000000000 
+SLEEP_TIME = 60
 
 class MinaTelegram():
     def __init__( self, config_file='config.ini' ):
@@ -9,15 +19,43 @@ class MinaTelegram():
         self.name = os.uname()[1]
         self.read_config( config_file )
         self.setup_telegram()
-        self.public_key = self.config['Mina']['publicKey']
-        self.graphql_api = self.config['GraphQL']['api']
+        self.public_key = self.config['Mina']['public_key']
+        self.client = bigquery.Client()
+        
+        # set the bigquery variable
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.config['BigQuery']['credentials'] 
         
         # hello message 
-        self.send( f'{self.name}: Hello from Mina Transaction Watcher!' )
-        response = self.get_transactions( { 'public_key': self.public_key,
-                                            'limit': 10 } )    
-        for tx in response:
-            print( tx )
+        self.send( f'{self.name}: Hello from Mina Watcher!' )
+        
+        while True:
+            # obtain blocks
+            blocks = self.get_blocks( self.config['Mina']['last_block'] )    
+            block_list = list( set( [int(i) for i in blocks['blockheight'].values[:]] ) )
+            block_list.sort()
+
+            # save the datetime
+            blocks['datetime'] = blocks['datetime'].apply(lambda x : pd.to_datetime(str(x)))
+            blocks['receivedtime'] = blocks['receivedtime'].apply(lambda x : pd.to_datetime(str(x)))
+            blocks['date'] = blocks['datetime'].dt.date
+            blocks['time'] = blocks['datetime'].dt.time
+            blocks['received_date'] = blocks['receivedtime'].dt.date
+            blocks['received_time'] = blocks['receivedtime'].dt.time
+            blocks['delta_time'] = blocks['receivedtime'] - blocks['datetime']
+            blocks['delta_time'] = blocks['delta_time'].apply(lambda x : x.total_seconds())
+
+            # parse the blocks
+            for blockheight in block_list:
+                # obtain all the blocks of the block height
+                blocks_of_height = blocks.loc[blocks['blockheight'] == blockheight] 
+                self.parse_blocks( blocks_of_height )
+
+            # save / update the config file
+            self.config['Mina']['last_block'] = str( blocks['blockheight'].max() )
+            with open( config_file, 'w') as configfile:
+                self.config.write(configfile)
+            
+            time.sleep( SLEEP_TIME )
         
     def read_config( self, config_file ):
         '''
@@ -31,74 +69,96 @@ class MinaTelegram():
         '''
         Setup telegram
         '''
-        self.telegramToken = self.config['Telegram']['telegramToken']
-        self.telegramChatId = self.config['Telegram']['telegramChatID']
+        self.telegram_token = self.config['Telegram']['telegram_token']
+        self.telegram_chat_id = self.config['Telegram']['telegram_chat_id']
 
     def send( self, msg ):
         '''
         Send telegram message
         '''
-        requests.post( f'https://api.telegram.org/bot{self.telegramToken}/sendMessage?chat_id={self.telegramChatId}&text={msg}' )
+        requests.post( f'https://api.telegram.org/bot{self.telegram_token}/sendMessage?chat_id={self.telegram_chat_id}&text={msg}' )
 
-    def _graphql_request(self, query: str, variables: dict = {}):
-        '''
-        GraphQL queries all look alike, this is a generic function to facilitate a GraphQL Request.
-        Arguments: query {str} -- A GraphQL Query
-        Keyword Arguments: variables {dict} -- Optional Variables for the GraphQL Query (default: {{}})
-        Raises: Exception: Raises an exception if the response is anything other than 200.
-        Returns: dict -- Returns the JSON Response as a Dict.
-        '''
-        # Strip all the whitespace and replace with spaces
-        query = " ".join(query.split())
-        payload = {'query': query}
-        if variables:
-            payload = {**payload, 'variables': variables}
+    def get_blocks( self, target_blockheight ):
+        '''Get the blocks'''
+        query = """
+        SELECT blockheight,
+            creator,
+            canonical,
+            datetime,
+            receivedtime,
+            transactions,
+            statehash,
+        FROM minaexplorer.archive.blocks
+        WHERE blockheight > %s
+        ORDER BY blockheight DESC""" % target_blockheight
+        query_job = self.client.query(query)
+        iterator = query_job.result()
+        rows = list(iterator)
 
-        headers = {"Accept": "application/json"}
-        response = requests.post(self.graphql_api,
-                                json = payload,
-                                headers = headers)
-        print( payload )
-        resp_json = response.json()
-        if response.status_code == 200 and "errors" not in resp_json:
-            return resp_json
-        else:
-            print(response.text)
-            raise Exception("Query failed -- returned code {}. {}".format(
-                response.status_code, query))
+        # Transform the rows into a nice pandas dataframe
+        df = pd.DataFrame(data=[list(x.values()) for x in rows], columns=list(rows[0].keys()))
+        df.drop_duplicates(subset=['statehash'])
+        df.sort_values(by=['blockheight'], inplace=True)
+        return df
 
-    def get_transactions( self, variables ):
-        '''
-        obtain the last transactions
-        '''
-        query = '''query($public_key: String!, $limit: Int){
-                transactions(limit: $limit, sortBy: DATETIME_DESC, query: {
-                    canonical: true,
-                    OR: [{
-                    to: $public_key
-                    }, {
-                    from: $public_key
-                    }]
-                }) {
-                    fee
-                    from
-                    to
-                    nonce
-                    amount
-                    memo
-                    hash
-                    kind
-                    dateTime
-                    block {
-                    blockHeight
-                    stateHash
-                    }
-                }
-            }
-        '''
-        print( query )
+    def parse_blocks( self, blocks ):
+        '''parse the blocks of the same blockheight'''
+        # parse the canonical
+        blocks.sort_values(by=['canonical'], inplace=True, ascending=False)
+        for index, block in blocks.iterrows():
+            self.parse_block( block )
 
-        return self._graphql_request(query, variables)['data']['transactions']
+    def parse_block( self, block ):
+        '''parse the block'''
+        # canonical flag
+        canonical = 'Canonical' if block.canonical == True else 'Non-Canonical'
+        # parse the transactions
+        transactions = self.parse_transactions( block.transactions )
+
+        # check if the creator of the block is the public_key
+        if block.creator == self.public_key:
+            self.send( f"{canonical} {block.blockheight}: {transactions['coinbase_receiver']} Received {transactions['coinbase_reward']} at { block.date } { block.time } [ { block.delta_time } ]" )
+
+        # check the transactions
+        if canonical == 'Canonical':
+            for transaction in transactions['user_commands']:
+                if self.public_key in [transaction['from'], transaction['to']]:
+                    self.send( f"{canonical} {block.blockheight}: {transaction['kind'].capitalize()} from {transaction['from']} to {transaction['to']} for {transaction['amount']} [{transaction['memo'].strip()}] at { block.date } { block.time } [ { block.delta_time } ]" )
+
+
+    def parse_transactions( self, transactions ):
+        '''parse transactions'''
+        if transactions['feetransfer'] == None or transactions['usercommands'] == None:
+            return { 'coinbase_reward' : 0,
+                     'coinbase_receiver' : '',
+                     'user_commands' : [] }
+        
+        output = {}
+        # parse the rewards
+        output['coinbase_reward'] = transactions['coinbase'] * MINA_DECIMALS
+        output['coinbase_receiver']= transactions['coinbasereceiveraccount']['publickey']
+        output['fee'] = []
+
+        # parse the fee transfers
+        for fee_transfer in json.loads( transactions['feetransfer'] ):
+            output['fee'].append( fee_transfer['fee'] * MINA_DECIMALS )
+
+        # user commands
+        output['user_commands'] = []
+        for user_command in json.loads( transactions['usercommands'] ):
+            user_tx = { 'from' : user_command['from'],
+                        'to' :   user_command['to'],
+                        'amount': round( user_command['amount'] * MINA_DECIMALS, 4 ),
+                        'fee' : round( user_command['fee'] * MINA_DECIMALS, 4),
+                        'kind' : user_command['kind'],
+                        'memo': self.decode_memo( user_command['memo'] ) }
+            output['user_commands'].append( user_tx )
+        return output
+
+    def decode_memo( self, memo ):
+        '''decode the memo'''
+        decoded = base58.b58decode( memo )[2:-4]
+        return decoded.decode("utf-8", "strict")
 
 mina_bot = MinaTelegram()
 
