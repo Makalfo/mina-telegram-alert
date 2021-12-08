81f5fbdcd449461e25722cb8c70501e7cdd86d92
Makalfo <info@chthonia.io>
2021-12-08 05:37:46 +0000

    add identifiers for addresses

--- mina-telegram-alert.py
+++ mina-telegram-alert.py
@@ -5,6 +5,7 @@ import pandas as pd
 import json
 import base58
 import time
+import urllib.request
 from google.cloud import bigquery
 
 pd.options.mode.chained_assignment = None 
@@ -24,7 +25,10 @@ class MinaTelegram():
         
         # set the bigquery variable
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.config['BigQuery']['credentials'] 
-        
+
+        # get the provider info
+        self.providers = self.get_providers( )
+
         # hello message 
         self.send( f'{self.name}: Hello from Mina Watcher!' )
         
@@ -114,6 +118,41 @@ class MinaTelegram():
         df.sort_values(by=['blockheight'], inplace=True)
         return df
 
+    def get_providers( self ):
+        '''get provider list'''
+        output = dict()
+        # staketab providers
+        with urllib.request.urlopen(self.config['Providers']['staketab']) as url:
+            data = json.loads(url.read().decode())
+        for provider in data['staking_providers']:
+            output[ provider['provider_address'] ] = provider['provider_title'] 
+
+        # Mina Foundation
+        mf_data = self.get_csv( self.config['Providers']['mina_foundation'] )
+        for idx, address in enumerate(mf_data):
+            output[ address ] = f'Mina Foundation {idx}'
+
+        # O1 Labs
+        mf_data = self.get_csv( self.config['Providers']['o1_labs'] )
+        for idx, address in enumerate(mf_data):
+            output[ address ] = f'O1 Labs {idx}'
+
+        return output
+
+    def get_csv( self, url ):
+        '''return the csv as a list'''
+        req = urllib.request.Request( url )
+        req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0')
+        content = urllib.request.urlopen(req)
+        data = pd.read_csv(content, header=None)
+        return list( data[0] ) 
+
+    def get_provider( self, address ):
+        '''return the provider name if it is in the provider dictionary'''
+        if address in self.providers.keys():
+            address = self.providers[ address ]
+        return address
+
     def parse_blocks( self, blocks ):
         '''parse the blocks of the same blockheight'''
         # parse the canonical
@@ -130,13 +169,17 @@ class MinaTelegram():
 
         # check if the creator of the block is the public_key
         if block.creator == self.public_key:
-            self.send( f"{canonical} {block.blockheight}: {transactions['coinbase_receiver']} Received {transactions['coinbase_reward']} at { block.date } { block.time } [ { block.delta_time } ]" )
+            self.send( f"{canonical} {block.blockheight}: Created Block - {self.get_provider( transactions['coinbase_receiver'] )} Received {transactions['coinbase_reward']} at { block.date } { block.time } [ { block.delta_time } ]" )
 
         # check the transactions
         if canonical == 'Canonical':
             for transaction in transactions['user_commands']:
                 if self.public_key in [transaction['from'], transaction['to']]:
-                    self.send( f"{canonical} {block.blockheight}: {transaction['kind'].capitalize()} from {transaction['from']} to {transaction['to']} for {transaction['amount']} [{transaction['memo'].strip()}] at { block.date } { block.time } [ { block.delta_time } ]" )
+                    # if it is a delegation, omit the amount
+                    if transaction['kind'] == 'STAKE_DELEGATION':
+                        self.send( f"{canonical} {block.blockheight}: Stake Delegation from {self.get_provider( transaction['from'] )} to {self.get_provider( transaction['to'])} [{transaction['memo'].strip()}] at { block.date } { block.time }" )
+                    else:
+                        self.send( f"{canonical} {block.blockheight}: {transaction['kind'].capitalize()} from {self.get_provider( transaction['from'] )} to {self.get_provider( transaction['to'])} for {transaction['amount']} [{transaction['memo'].strip()}] at { block.date } { block.time }" )
 
 
     def parse_transactions( self, transactions ):
